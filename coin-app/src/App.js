9aca5624419d8e1d0a0bfa22ce08d692d86391b1
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-05-05 16:40:12 -0600

    just messing around

--- coin-app/src/App.js
+++ coin-app/src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>Lucky Penny App</h1>
+      <div className='penny-lore'>
+        <h3>The Luck of The Penny</h3>
+        <p>According to the book and the lore surrounding them, pennies are especially lucky when you find them face up, as that means that you have more money on the way. Finding a penny after rainfall could mean that the pennies came from heaven or from the gods. </p>
+        <h3>Is Tails Up Unlucky?</h3>
+        <p>Some people believe that picking up a penny that is tails up will bring you bad luck, and only the face side of the coin is lucky. Rather than take the tails-up penny and risk whatever ill fortune that may bring, there’s a salt-over-the-shoulder way of dealing with it. The traditional way of dealing with it is to lean down and turn the penny over so the head is facing up, but then to leave it so the good luck of the face-up penny can be bestowed upon the next passerby.</p>
+      </div>
     </div>
   );
 }
