01e8eeb82c94433ab6a74920706d8aa792200157
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-07-08 19:29:58 -0400

    began OrderForm.js

--- coin-app/src/components/OrderForm.js
+++ coin-app/src/components/OrderForm.js
@@ -1,1 +1,20 @@
+import React, { useState } from "react";
 import "./OrderForm.css";
+
+const initialFormValues = {
+  name: "",
+  email: "",
+  street_address: "",
+  city: "",
+  state: "",
+  zip_code: "",
+  phone_number: "",
+};
+
+const OrderForm = () => {
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  return <div></div>;
+};
+
+export default OrderForm;
