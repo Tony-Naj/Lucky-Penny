526b1ea1519fa6db1cdf544502c0416c0dee8c4a
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-10-29 12:19:42 -0400

    changed tablet view to column instead of row to improve readability

--- coin-app/src/components/DatePage.js
+++ coin-app/src/components/DatePage.js
@@ -12,12 +12,7 @@ function DatePage(props) {
       </div>
       <div className="info">
         <div className="events">
-          <h3 className="events-h3">
-            {`These historic moments happened on ${date}:`}
-            {/* {`On ${date}, ${
-            events[events.length - 1].text
-          } Here are some more events that took place on ${date}:`} */}
-          </h3>
+          <h3 className="events-h3">{`Moments in history from ${date}th:`}</h3>
           <ul>
             {events.map((event) => (
               <li className="list">
@@ -26,15 +21,8 @@ function DatePage(props) {
             ))}
           </ul>
         </div>
-
         <div className="births">
-          <h3 className="births-h3">
-            {`These people of renown were born on ${date}:`}
-            {/* <h3 className="births-h3">{`Did you know 
-            ${
-              births[births.length - 1].text
-            } was born on ${date}? Here are more notable births:`}</h3> */}
-          </h3>
+          <h3 className="births-h3">{`Notable birthdays on ${date}th:`}</h3>
           <ul>
             {births.map((birth) => (
               <li className="list">
